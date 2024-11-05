4d20528f118270c8ca2ac5482221bebb0ee1804c
AISHWARYASN07 <116338015+AISHWARYASN07@users.noreply.github.com>
2024-11-05 16:34:08 +0530

    Update app.py

--- Choco_house/chocolate_house/app.py
+++ Choco_house/chocolate_house/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import traceback
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chocolate_house.db"
 app.config["SECRET_KEY"] = "my_secrete_key@123*-"  
 db = SQLAlchemy(app)
@@ -56,12 +56,13 @@ def edit_flavor(id):
     flavor = SeasonalFlavor.query.get_or_404(id)
     if request.method == "POST":
         flavor.name = request.form["name"]
-        flavor.description = request.form.get("description", "")
+        flavor.description = request.form["description"]
         flavor.ingredients = request.form["ingredients"]
         db.session.commit()
         flash("Seasonal flavor updated successfully!", "success")
         return redirect(url_for("seasonal_flavors"))
     return render_template("edit_flavor.html", flavor=flavor)
+
 
 @app.route("/delete-flavor/<int:id>", methods=["POST"])
 def delete_flavor(id):
@@ -138,7 +139,7 @@ def allergy_concerns():
     concerns = AllergyConcern.query.all()
     return render_template("allergy_concerns.html", concerns=concerns)
 
-if __name__== "__main__":
+if _name_ == "_main_":
     with app.app_context():
         db.create_all()  # Ensure tables are created
     app.run(debug=True)
